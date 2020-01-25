572a3080be76157be57c2ec28f1a7aa2c267efd7
Ryan Churchill <ryan.churchill@gmail.com>
2020-01-24 21:44:43 -0800

    player is walking

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,17 +4,31 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // config
     [SerializeField] float MovementSpeed = 1.0f;
+
+    // cached components
+    Rigidbody2D myRigidBody;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRigidBody = GetComponent<Rigidbody2D>();
+
+        StartMoving();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 playerVelocity = new Vector2(0, MovementSpeed);
+        myRigidBody.velocity = playerVelocity;
+    }
+
+    void StartMoving()
+    {
+    //    Vector2 playerVelocity = new Vector2(0, MovementSpeed);
+    //    myRigidBody.velocity = playerVelocity;
+        //myRigidBody.AddForce(new Vector2(0, MovementSpeed));
     }
 }
