ee85655942a3d6ba8dec39560c079af08ad6524e
Ryan Churchill <ryan.churchill@gmail.com>
2020-01-26 13:18:17 -0800

    Custom camera

---
+++ Assets/Scripts/MainCamera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    // config
+    [SerializeField] GameObject player;
+
+    // other
+    float playerStartingY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerStartingY = player.transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 cameraPosition = transform.position;
+        cameraPosition.y = player.transform.position.y - playerStartingY;
+        transform.position = cameraPosition;
+        print(cameraPosition);
+    }
+}
