56b17a1289fbcb4663360eae67bc19181df43cac
Ryan Churchill <ryan.churchill@gmail.com>
2020-01-26 15:13:10 -0800

    Score Display increases with time

---
+++ Assets/Scripts/GameSession.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class GameSession : MonoBehaviour
+{
+    // config
+    [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] float scorePerSecond = 1.0f;
+    [SerializeField] int updateDisplayAfterMs = 1000;
+
+    // other
+    float score = 0;
+    DateTime timeOfLastScoreDisplayUpdate = DateTime.Now;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        IncreaseScore(scorePerSecond * Time.deltaTime);
+    }
+
+    void IncreaseScore(float pointsToAdd)
+    {
+        UpdateScore(score + pointsToAdd);
+    }
+
+    void UpdateScore(float newScore, bool forceDisplayUpdate = false)
+    {
+        score = newScore;
+
+        if (forceDisplayUpdate || DateTime.Now.Subtract(timeOfLastScoreDisplayUpdate).TotalMilliseconds > updateDisplayAfterMs)
+        {
+            timeOfLastScoreDisplayUpdate = DateTime.Now;
+            scoreText.text = String.Format("{0:#,###0}", score);
+        }
+        
+    }
+}
