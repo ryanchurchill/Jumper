a01a480d0a4cb91df312970929bc4e362b66ee57
Ryan Churchill <ryan.churchill@gmail.com>
2020-02-20 21:26:10 -0800

    infinite boring level

---
+++ Assets/Scripts/LevelSegment.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSegment : MonoBehaviour
+{
+    const float SegmentSize = 15;
+    bool hasSpawnedNextSegment = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Player player = collision.GetComponent<Player>();
+        if (player && !hasSpawnedNextSegment)
+        {
+            //Debug.Log("player collision with Segment");
+            Instantiate(
+                this,
+                new Vector2(
+                    this.transform.position.x,
+                    this.transform.position.y + SegmentSize
+                ),
+                Quaternion.identity
+            );
+
+            hasSpawnedNextSegment = true;
+        }
+
+    }
+}
